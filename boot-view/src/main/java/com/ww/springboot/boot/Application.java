7a699bc4db8382389fbbe4fdd2e7256febaee027
wanwei <1254837494@qq.com>
2018-11-06 11:21:58 +0800

    连接注册中心及配置中心，以及引包问题的解决

--- boot-view/src/main/java/com/ww/springboot/boot/Application.java
+++ boot-view/src/main/java/com/ww/springboot/boot/Application.java
@@ -2,12 +2,14 @@ package com.ww.springboot.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @SpringBootApplication
+@EnableEurekaClient
 @ComponentScan(basePackages = "com.ww.springboot.boot.*")
 public class Application {
 	

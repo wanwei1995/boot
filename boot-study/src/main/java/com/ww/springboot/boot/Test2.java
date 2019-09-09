900e6efcbb1a14db3b628461118396b7040cf2be
万伟 <1254837494@qq.com>
2019-09-09 16:16:03 +0800

    依赖去除

--- boot-study/src/main/java/com/ww/springboot/boot/Test2.java
+++ boot-study/src/main/java/com/ww/springboot/boot/Test2.java
@@ -1,26 +1,26 @@
 package com.ww.springboot.boot;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 public class Test2 {
 	
 	public static void main(String[] args) {
-		ArrayList<String> list = new ArrayList<String>();  
-		list.add("one");  
-		list.add("two");  
-		list.add("two");  
-		list.add("two");  
-		list.add("two");  
-		Iterator<String> iterator = list.iterator();
-		while(iterator.hasNext()){
-			String s = iterator.next();
-			if(s.equals("two")){
-				iterator.remove();
-			}
-		}
-		System.out.println(list);
+		Map<String,Long> map = new HashMap<>();
 
+		map.put("a",3L);
+		System.out.println(map.get("a"));
+		map.put("a",4L);
+		map.put("a",5L);
+		System.out.println(map.get("a"));
+		map.put("a",6L);
+		System.out.println(map.get("a"));
+		map.put("a",7L);
+
+		System.out.println(map.get("a"));
 		//测试
 	}
 
